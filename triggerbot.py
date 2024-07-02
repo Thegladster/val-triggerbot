99131f00dbace3f00159a2e723109656794f4bd0
Thegladster <64565266+Thegladster@users.noreply.github.com>
2024-07-02 12:04:59 -0700

    Update triggerbot.py

--- triggerbot.py
+++ triggerbot.py
@@ -3,7 +3,7 @@ import keyboard
 import numpy as np
 from PIL import ImageGrab
 import time
-
+import sys
 
 class Head:
     def __init__(self, xmin, ymin, xmax, ymax):
@@ -23,9 +23,95 @@ def check_for_yellow(screenshot):
     return np.column_stack(np.where(yellow))
 
 
+def select():
+    global interval, loop, delay, rounds
+
+    menu_select = input('Choose what gun you are using (all lowercase), or type "exit" to exit this program. ')
+
+    if menu_select == 'exit' or menu_select == '9exit':
+        loop = False
+
+    elif menu_select == 'classic':
+        interval = 3
+        rounds = 6.75
+
+    elif menu_select == 'shorty':
+        interval = 1
+        rounds = 3.33
+
+    elif menu_select == 'frenzy':
+        interval = 4
+        rounds = 10
+
+    elif menu_select == 'ghost':
+        interval = 5
+        rounds = 6.75
+
+    elif menu_select == 'sheriff':
+        interval = 1
+        rounds = 4
+
+    elif menu_select == 'stinger':
+        interval = 5
+        rounds = 16
+
+    elif menu_select == 'spectre':
+        interval = 5
+        rounds = 13.33
+
+    elif menu_select == 'bucky':
+        interval = 1
+        rounds = 1.1
+
+    elif menu_select == 'judge':
+        interval = 1
+        rounds = 3.5
+
+    elif menu_select == 'bulldog':
+        interval = 3
+        rounds = 10
+
+    elif menu_select == 'guardian':
+        interval = 3
+        rounds = 5.25
+
+    elif menu_select == 'phantom':
+        interval = 4
+        rounds = 11
+
+    elif menu_select == 'vandal':
+        interval = 3
+        rounds = 9.75
+
+    elif menu_select == 'marshal':
+        interval = 1
+        rounds = 1.5
+
+    elif menu_select == 'outlaw':
+        interval = 2
+        rounds = 2.75
+
+    elif menu_select == 'operator':
+        interval = 1
+        rounds = 0.6
+
+    elif menu_select == 'ares':
+        interval = 4
+        rounds = 13
+
+    elif menu_select == 'odin':
+        interval = 12
+        rounds = 12
+
+    else:
+        print('Not a valid statement.')
+        select()
+
+    delay = (interval / rounds) / interval
+
 # Initializing color bounds for yellow
-lower_yellow = np.array([200, 200, 0])
-upper_yellow = np.array([255, 255, 200])
+lower_yellow = np.array([230, 230, 0])
+upper_yellow = np.array([255, 255, 100])
 
 # Grabbing image through screenshot to find size of monitor
 shot = ImageGrab.grab()
@@ -33,7 +119,7 @@ w, h = shot.size
 center = [w / 2, h / 2]
 
 # Change variables depending on the size of the screenshot you want to be taken
-width = 500
+width = 250
 height = 10
 
 # Sets image in the center with given width and height
@@ -42,23 +128,32 @@ left, top, right, bottom = int(left), int(top), int(right), int(bottom)
 region = (left, top, right, bottom)
 print(region)
 
-# Initializes DirectX camera
-camera = dxcam.create(device_idx=0, output_idx=0, region=region)
-camera.start(target_fps=120)
+loop = True
+select()
 
-while not keyboard.is_pressed('9'):
+# Initializes DirectX camera (FPS should not exceed 160)
+camera = dxcam.create(device_idx=0, output_idx=0, region=region)
+camera.start(target_fps=144, video_mode=True)
+
+while loop:
+
+    if keyboard.is_pressed('ctrl+p'):
+        select()
+
     frame = camera.get_latest_frame()
 
     # Finds all yellow points
     coordinates = check_for_yellow(frame)
 
-    # Given that there is yellow, finds maxes and mins
+    # Given that there is yellow, finds maxes and minimums
     if len(coordinates) > 0:
         min_y, min_x = np.min(coordinates, axis=0)
         max_y, max_x = np.max(coordinates, axis=0)
 
         # Sometimes the yellow on enemy head is only on one side or there are multiple enemies
         if max_x - min_x < 10 or max_x - min_x > 150:
+
+            # Just assumes that the head is 30 pixels by 30 pixels
             head = Head(min_x, min_y, min_x + 30, min_y + 30)
             x_distance, y_distance = abs(head.xcenter - width / 2), abs(head.ycenter - height / 2)
         else:
@@ -66,10 +161,13 @@ while not keyboard.is_pressed('9'):
             x_distance, y_distance = abs(head.xcenter - width / 2), abs(head.ycenter - height / 2)
 
         # Checks if crosshair is on head
-        if x_distance < (head.width / 2) and y_distance < head.height / 2:
-            keyboard.press_and_release('0')
-            time.sleep(0.25)
+        if x_distance < (head.width / 2) and y_distance < (head.height / 2):
+            for i in range(interval):
+                keyboard.press_and_release('0')
+                time.sleep(delay)
+
+            time.sleep(0.5)
 
 
-print('Process over.')
+print('Triggerbot completed.')
 camera.stop()
